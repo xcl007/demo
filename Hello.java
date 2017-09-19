e20eb3fe0bea50142f64aa9ee8f42de9bf218376
xcl <3082507239@qq.com>
2017-09-19 11:58:48 +0800

    Modify Hello.java File And Add New File Emp.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello
 {
-	public static void main(string args[]){
-	 System.out.println("3333333333")
+	public static void main(string[] args){
+	 System.out.println("3333333333");
+	 System.out.println("444444444");
 	}
 }
